1421f3582cb243311b52ad505cfeda6d914e1ab7
chris <chris.gresock@gmail.com>
2019-04-07 12:01:05 -0400

    added initial methods and removed main testing method

--- decrypt.go
+++ decrypt.go
@@ -1,4 +1,4 @@
-package main
+package railscookie
 
 import (
 	"fmt"
@@ -8,10 +8,6 @@ import (
 	"net/url"
 	"github.com/spacemonkeygo/openssl"
 	"encoding/base64"
-)
-
-const (
-	salt = "authenticated encrypted cookie"
 )
 
 type enc_cookie struct {
@@ -42,7 +38,11 @@ func get_enc_cookie(raw string) (ec enc_cookie, err error){
 	return enc_cookie{body: body, iv: iv, auth_tag: auth_tag }, nil
 }
 
-func Rails(cookie string, secret_key_base []byte) {
+func verify(cookie enc_cookie) {}
+
+func decrypt(cookie enc_cookie) {}
+
+func DecryptAndVerify(cookie string, secret_key_base []byte) {
 	message, err := get_enc_cookie(cookie)
 	secret := p.Key(secret_key_base, []byte(salt), 1000, 32, sha1.New) 
 	ctx, err := openssl.NewGCMDecryptionCipherCtx(len(secret)*8, nil, secret, message.iv)
@@ -53,10 +53,3 @@ func Rails(cookie string, secret_key_base []byte) {
 	data, err := ctx.DecryptUpdate(message.body)
 	fmt.Println(err)
 	fmt.Println(string(data))}
-
-func main() {
-	cookie := "6g0bK4or%2FsOhFzOiOEkwQ9YneH1wvHMfdqH5ZsFlhU8qDNTqC4hZ7h%2FjxUu6Q%2FHxSjRnDIl3XFql8JQC25XWWJDYiI%2F7enasOZAAoC7xVVqRVbuJip7LKBnp9he511kOMcSruaID2Hl%2BO8QJEyz2B99OHL3wRoUKep7upjO5dPfOhmCJtiAAZdoKRHnE0SA6FG9bI%2FWLVMnvFAL7yLKaHYdR1spcs6NJ%2FTIWZWs0tYMSGC1c6PoOzsXY94fiykZEJ8%2F9yizWMlVwb1F%2Fl94E8cKxNjTeasUqCRx2hQx6Jit6srPyX6qm6cQVx0bnk2zieA%3D%3D--lYGRqA1Q707vn8rp--RKgU59EtqNE%2BKQlu4cgmuA%3D%3D"
-	secret_key_base := []byte("ef0eee90f56ce0149fd9bce4a014aa239bfdb3dfac6c9428a86b5b1a93f217675e471fe8066265bd662b7f2dad76ba77ed1bb69dcebde439915c8e8ae4cb6e69")
-	Rails(cookie, secret_key_base)
-	fmt.Println("testing")
-}
---
+++ shared.go
@@ -0,0 +1,5 @@
+package railscookie
+
+const (
+	salt = "authenticated encrypted cookie"
+)
